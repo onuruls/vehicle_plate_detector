1cf00f3f2856abd7b6ea6a8a68bee7244182dd6d
Markus Artemov <markus.artemov@stud.hs-flensburg.de>
2024-07-03 20:28:15 +0200

    Fix templategenerator bug

--- backend/src/main/java/com/bv/onur_markus/vehicle_plate_detector/repository/CityCodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bv.onur_markus.vehicle_plate_detector.repository;
-
-import com.bv.onur_markus.vehicle_plate_detector.model.CityCode;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface CityCodeRepository extends MongoRepository<CityCode, String> {
-    Optional<CityCode> findByCode(String code);
-}
